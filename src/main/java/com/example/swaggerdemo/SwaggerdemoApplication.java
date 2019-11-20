5bbc147591eda2008bcffc3d454fd47f763a9b15
zuoqiang <zuoqiang@lianlianpay.com>
2019-11-20 13:46:33 +0800

    swagger 启动

---
+++ src/main/java/com/example/swaggerdemo/SwaggerdemoApplication.java
@@ -0,0 +1,20 @@
+package com.example.swaggerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * https://www.ibm.com/developerworks/cn/java/j-using-swagger-in-a-spring-boot-project/index.html
+ * <p>
+ * ibm swagger 教学文档
+ *
+ * @author zuoqiang
+ */
+@SpringBootApplication
+public class SwaggerdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwaggerdemoApplication.class, args);
+    }
+
+}
